c2d630283ba0b571699684b7aba5409f30bf156f
lake <102535481@qq.com>
2020-12-15 11:08:08 +0800

    ty

--- src/main.ts
+++ src/main.ts
@@ -7,28 +7,35 @@ import axios from './plugins/axios'
 import { registerMicroApps, start } from 'qiankun'
 
 
+let app: any = null
+const render = ({ loading }) => {
+    if (!app) {
+        app = createApp(App).use(store).use(router).use(axios)
+        installElementPlus(app)
+        app.mount('#rootApp')
+    } else {
+        store.commit('setLoading', loading);
+    }
+}
+const loader = loading => render({ loading });
 registerMicroApps([
     {
         name: 'jb-operator',
         entry: 'http://localhost:8082',
         container: '#vue',
+        loader,
         activeRule: '/operator'
     }
 ], {
     beforeMount: app => {
         console.log('beforeMount');
-        store.commit('setLoading', true)
         return Promise.resolve()
     },
     afterMount: app => {
         console.log('afterMount');
-        store.commit('setLoading', false)
         return Promise.resolve()
     }
 })
 
+render({ loading: true })
 start()
-
-const app = createApp(App).use(store).use(router).use(axios)
-installElementPlus(app)
-app.mount('#rootApp')
